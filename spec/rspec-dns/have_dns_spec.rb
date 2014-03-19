aed558064abc34141a77f2607ab8e0170a0aa85a
Alexey Lapitsky <alexey@spotify.com>
2014-03-19 10:54:28 -0400

    switch to dnsruby, major version bump

--- spec/rspec-dns/have_dns_spec.rb
+++ spec/rspec-dns/have_dns_spec.rb
@@ -1,13 +1,24 @@
 require 'spec_helper'
-require_relative '../../lib/rspec-dns/have_dns'
+require 'dnsruby'
+require 'resolv'
+require_relative '../../lib/rspec-dns'
+
+def stub_records(strings)
+  records = strings.map { |s| Dnsruby::RR.new_from_string(s) }
+  resolver = Dnsruby::Resolver.new
+  Dnsruby::Resolver.stub(:new) do
+    yield if block_given?
+    resolver
+  end
+  resolver.stub_chain(:query, :answer).and_return(records)
+end
 
 describe 'rspec-dns matchers' do
 
   describe '#have_dns' do
     context 'with a sigle record' do
       it 'can evalutate an A record' do
-        records = [Resolv::DNS::Resource::IN::A.new('192.168.100.100')]
-        Resolv::DNS.stub_chain(:new, :getresources).and_return(records)
+        stub_records(['example.com 86400 A 192.168.100.100'])
 
         'example.com'.should have_dns.with_type('A')
         'example.com'.should_not have_dns.with_type('TXT')
@@ -15,8 +26,7 @@ describe 'rspec-dns matchers' do
       end
 
       it 'can evalutate a AAAA record' do
-        records = [Resolv::DNS::Resource::IN::AAAA.new('2001:0002:6c::430')]
-        Resolv::DNS.stub_chain(:new, :getresources).and_return(records)
+        stub_records(['example.com 86400 AAAA 2001:0002:6c::430'])
 
         'example.com'.should have_dns.with_type('AAAA')
         'example.com'.should_not have_dns.with_type('A')
@@ -25,8 +35,7 @@ describe 'rspec-dns matchers' do
       end
 
       it 'can evalutate a CNAME record' do
-        records = [Resolv::DNS::Resource::IN::CNAME.new('www.example.com')]
-        Resolv::DNS.stub_chain(:new, :getresources).and_return(records)
+        stub_records(['www.example.com 300 IN CNAME example.com'])
 
         'example.com'.should have_dns.with_type('CNAME')
         'example.com'.should_not have_dns.with_type('AAAA')
@@ -34,8 +43,7 @@ describe 'rspec-dns matchers' do
       end
 
       it 'can evalutate an MX record' do
-        records = [Resolv::DNS::Resource::IN::MX.new(40, 'mail.example.com')]
-        Resolv::DNS.stub_chain(:new, :getresources).and_return(records)
+        stub_records(['example.com. 7200 MX 40 mail.example.com.'])
 
         'example.com'.should have_dns.with_type('MX')
         'example.com'.should_not have_dns.with_type('CNAME')
@@ -48,60 +56,43 @@ describe 'rspec-dns matchers' do
           .and_preference(40).and_exchange('example.com')
       end
 
-      it 'can evalutate a CNAME record' do
-        records = [Resolv::DNS::Resource::IN::NS.new('ns.sub.example.com')]
-        Resolv::DNS.stub_chain(:new, :getresources).and_return(records)
+      it 'can evalutate an NS record' do
+        stub_records(['sub.example.com. 300 IN NS ns.sub.example.com.'])
 
         'example.com'.should have_dns.with_type('NS')
         'example.com'.should_not have_dns.with_type('MX')
-        'example.com'.should have_dns.with_type('NS').and_name('ns.sub.example.com')
-        'example.com'.should_not have_dns.with_type('NS').and_name('sub.example.com')
+        'example.com'.should have_dns.with_type('NS').and_domainname('ns.sub.example.com')
+        'example.com'.should_not have_dns.with_type('NS').and_domainname('sub.example.com')
       end
 
       it 'can evalutate a PTR record' do
-        records = [Resolv::DNS::Resource::IN::PTR.new('mail2.example.org')]
-        Resolv::DNS.stub_chain(:new, :getresources).and_return(records)
+        stub_records(['ptrs.example.com. 300 IN PTR ptr.example.com.'])
 
         '192.168.100.100'.should have_dns.with_type('PTR')
         '192.168.100.100'.should_not have_dns.with_type('MX')
-        '192.168.100.100'.should have_dns.with_type('PTR').and_name('mail2.example.org')
-        '192.168.100.100'.should_not have_dns.with_type('PTR').and_name('mail1.example.org')
+        '192.168.100.100'.should have_dns.with_type('PTR').and_domainname('ptr.example.com')
+        '192.168.100.100'.should_not have_dns.with_type('PTR').and_domainname('ptrs.example.com')
       end
 
       it 'can evalutate an SOA record' do
-        records = [Resolv::DNS::Resource::IN::SOA.new('ns.sub.example.com',
-                                                      'admin.sub.example.com',
-                                                      '2014012701',
-                                                      7200,
-                                                      1800,
-                                                      1209600,
-                                                      300)]
-
-        Resolv::DNS.stub_chain(:new, :getresources).and_return(records)
+        stub_records(['example.com 210 IN SOA ns.example.com a.example.com. 2014030712 60 25 3628800 900'])
 
         'example.com'.should have_dns.with_type('SOA')
         'example.com'.should_not have_dns.with_type('PTR')
         'example.com'.should have_dns.with_type('SOA')
-          .and_mname('ns.sub.example.com')
-          .and_rname('admin.sub.example.com')
-          .and_serial('2014012701')
-          .and_refresh(7200)
-          .and_retry(1800)
-          .and_expire(1209600)
-          .and_minimum(300)
+          .and_mname('ns.example.com')
+          .and_rname('a.example.com')
+          .and_serial('2014030712')
+          .and_refresh(60)
+          .and_retry(25)
+          .and_expire(3628800)
+          .and_minimum(900)
         'example.com'.should_not have_dns.with_type('SOA')
-          .and_mname('ns.sub.example.com')
-          .and_rname('admin.sub.example.com')
-          .and_serial('2014012601')
-          .and_refresh(7200)
-          .and_retry(1800)
-          .and_expire(1209600)
-          .and_minimum(300)
+          .and_mname('nstest.example.com')
       end
 
-      it 'can evalutate a CNAME record' do
-        records = [Resolv::DNS::Resource::IN::TXT.new('v=spf1 a:example.com ~all')]
-        Resolv::DNS.stub_chain(:new, :getresources).and_return(records)
+      it 'can evalutate a TXT record' do
+        stub_records(['example.com. 300 IN TXT "v=spf1 a:example.com ~all"'])
 
         'example.com'.should have_dns.with_type('TXT')
         'example.com'.should_not have_dns.with_type('SOA')
@@ -110,25 +101,19 @@ describe 'rspec-dns matchers' do
       end
     end
 
-    context 'with multiple records' do
-    end
-
     context 'with changable connection timeout' do
-      it 'should timeout within 2 seconds in default' do
-        records = [Resolv::DNS::Resource::IN::A.new('192.168.100.100')]
-        Resolv::DNS.stub_chain(:new, :getresources) { sleep 2 ; records }
-
-        $stderr.should_receive(:puts).with("Connection timed out for example.com")
+      it 'should timeout within 3 seconds in default' do
+        stub_records(['example.com 86400 A 192.168.100.100']) do
+          sleep 3
+        end
         'example.com'.should_not have_dns.with_type('A')
       end
 
-      it 'should not timeout within 2 seconds when timeout is 5' do
-        records = [Resolv::DNS::Resource::IN::A.new('192.168.100.100')]
-        Resolv::DNS.stub_chain(:new, :getresources) { sleep 2 ; records }
-
+      it 'should not timeout within 3 seconds when timeout is 5' do
         RSpec.configuration.rspec_dns_connection_timeout = 5
-
-        $stderr.should_not_receive(:puts).with("Connection timed out for example.com")
+        stub_records(['example.com 86400 A 192.168.100.100']) do
+          sleep 3
+        end
         'example.com'.should have_dns.with_type('A')
       end
     end
